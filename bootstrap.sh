e057c2ad72a153c27aa762410641daa855dc1faa
Amir Rachum <amir@rachum.com>
2016-10-02 10:49:35 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,8 @@ sudo apt-get install git python-pip libssl-dev
 sudo pip install --upgrade pip wheel ansible
 
 mkdir -p ~/code
-git clone git@github.com:Nurdok/dotfiles.git ~/code/dotfiles
+# Using HTTPS to avoid RSA key fingerprint prompt
+git clone https://github.com/Nurdok/dotfiles.git ~/code/dotfiles
 ansible-playbook ~/code/dotfiles/playbook.yml
 
 set +x
